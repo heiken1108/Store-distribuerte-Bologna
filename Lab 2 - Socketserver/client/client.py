3569ec323cc020b0e32c2fbbd6297ab271a39b59
Håkon Hoelsæter <hakon1108@gmail.com>
2024-09-26 19:22:08 +0200

    Prepared for delivery

--- Lab 2 - Socketserver/client/client.py
+++ Lab 2 - Socketserver/client/client.py
@@ -1,35 +1,58 @@
-from socket import * 
-import time
+from socket import socket, AF_INET, SOCK_STREAM
 import os
 
-def get_all_files():
-  all_files = []
-  all_files.append(os.listdir("clientFiles"))
-  
 
-print("Starting client")
+class MyClient:
+    def __init__(self, host="127.0.0.1", port=4449):
+        self.host = host
+        self.port = port
 
-try:
-  s = socket(AF_INET, SOCK_STREAM)
-  # Connect to the server
-  s.connect(("127.0.0.1", 4449))
+    def get_client_files(self):
+        return os.listdir("files")
 
-except:
-  print("Couldn't connect to the server.")
-  exit()
+    def get_all_files(self):
+        all_files = set()
+        all_files.update(self.get_client_files())
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        sock.send("ls".encode())
+        response = sock.recv(1024)
 
-while True:
-  choice = input("Which operation would you like to perform? (R: Read a file, Q: Quit) ")
-  if choice.lower() == "q":
-    s.close()
-    print("Connection closed.")
-    break
-  file_name = input("Enter the name of the file you want to read (including extension): ")
-  try:
-    s.send(f"{file_name}".encode())
-    server_response = s.recv(1024)
-    print(server_response.decode())
-    #break
-  except:
-    print("Something wrong happened.")
-    time.sleep(5)
+        files = eval(response.decode())
+        all_files.update(files)
+        return list(all_files)
+
+    def read_file(self, file_name):
+        with open(f"files/{file_name}", "r") as file:
+            print(file.read())
+
+    def run(self):
+        while True:
+            choice = input(
+                "Which operation would you like to perform? "
+                "(R: Read a file, Q: Quit) "
+            )
+            if choice == "Q":
+                break
+            elif choice == "R":
+                files = self.get_all_files()
+                print("Files available: ")
+                for i, file in enumerate(files):
+                    print(f" - {file}")
+                file_name = input(
+                    "Enter the filename of the file you want to read "
+                    "(including extension): "
+                )
+                if file_name in self.get_client_files():
+                    self.read_file(file_name)
+                else:
+                    sock = socket(AF_INET, SOCK_STREAM)
+                    sock.connect((self.host, self.port))
+                    sock.send(file_name.encode())
+                    response = sock.recv(1024)
+                    print(response.decode())
+
+
+if __name__ == "__main__":
+    client = MyClient(port=4449)
+    client.run()
--- Lab 2 - Socketserver/server/server.py
+++ Lab 2 - Socketserver/server/server.py
@@ -1,37 +1,66 @@
 from socket import *
+import os
 
-serversocket = socket(AF_INET, SOCK_STREAM)
+class Server:
+    def __init__(self, host='', port=4449):
+        self.host = host
+        self.port = port
+        self.server_socket = socket(AF_INET, SOCK_STREAM)
+        self.server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self.socketIsSet = False
 
-port = 4449
+        try:
+            self.server_socket.bind((self.host, self.port))
+            print(f"Server started on port {self.port}")
+        except OSError:
+            print("Port already in use")
+            raise
 
-try:
-  serversocket.bind(('', port))
-except:
-  print('Already in use')
+        self.server_socket.listen(1)
 
-serversocket.listen(1)
+    def handle_client_request(self, connection_socket: socket, address):
+        print(f"Handle request from {address}")
+        try:
+            message = connection_socket.recv(1024).decode()
+            print("Message received from client:", message)
+            if message is not None:
+                if message == 'ls':
+                    file_response = self.fetch_all_filenames()
+                else:
+                    file_response = self.fetch_file_content(filename=message)
+                connection_socket.send(file_response.encode())
+            else:
+                connection_socket.send("No message received".encode())
 
+        except (IOError, IndexError):
+            connection_socket.send("An error occurred when handling the request".encode())
+        finally:
+            connection_socket.close()
+            
+    def fetch_all_filenames(self):
+        return str(os.listdir("files"))
 
+    def fetch_file_content(self, filename):
+        try:
+            with open(f"files/{filename}", "r") as file:
+                output_data = file.read()
+                return output_data
+        except FileNotFoundError:
+            return "File not found"
 
-while True:
-  print('We are ready')
+    def start(self):
+        try:
+            while True:
+                connection_socket, address = self.server_socket.accept()
+                print(f"Connection established with {address}")
+                self.handle_client_request(connection_socket, address)
+        
+        except KeyboardInterrupt:
+            print("\nServer interrupted. Shutting down")
+            self.server_socket.close()
+            print("Server closed.")
+            
 
-  connectionSocket, address = serversocket.accept()
-  
-  try:
-    message = connectionSocket.recv(1024).decode()
-    print("Message:", message)
-    if message is not None:
-      filepath = message
-      print(filepath)
-      f = open(f"serverFiles/{filepath}", "r")
-      outputdata = f.read()
-      connectionSocket.send(outputdata.encode()) 
-      connectionSocket.close()
-  except(IOError, IndexError):
-    connectionSocket.send("Filen ble ikke funnet".encode())
-
-  connectionSocket.close()
-  
-
-serversocket.close()
+if __name__ == "__main__":
+    server = Server(port=4449)
+    server.start()
