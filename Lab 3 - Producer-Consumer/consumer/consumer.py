268ed83c5bccb1b740fd9eae566117f0d5922a5f
Håkon Hoelsæter <hakon1108@gmail.com>
2024-10-01 13:56:46 +0200

    Init Lab 3

---
+++ Lab 3 - Producer-Consumer/consumer/consumer.py
@@ -0,0 +1,9 @@
+class Consumer():
+  def __init__(self):
+    self.consumed = []
+
+  def consume(self):
+    pass
+
+if __name__ == "__main__":
+  print("Create consumer")
---
+++ Lab 3 - Producer-Consumer/producer/producer.py
@@ -0,0 +1,6 @@
+class Producer():
+  def __init__(self, even: bool):
+    self.even = even
+
+if __name__ == "__main__":
+  print("Create producer")
