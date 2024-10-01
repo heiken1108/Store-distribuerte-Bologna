4439dd31fbac3c3595be2ca89eaf3f9e47c23409
Jakob Punnerud <jepunnerud@gmail.com>
2024-10-01 19:11:23 +0200

    create producers for lab 3

--- Lab 3 - Producer-Consumer/producer/producer.py
+++ Lab 3 - Producer-Consumer/producer/producer.py
@@ -1,6 +1,40 @@
+from socket import socket, AF_INET, SOCK_STREAM
+import threading, time, random, json
+
 class Producer():
-  def __init__(self, even: bool):
+  def __init__(self, even: bool, lock: threading.Lock, host="192.168.1.2", port=4449):
+    self.host = host
+    self.port = port
     self.even = even
+    self.lock = lock
+
+  def send_data(self):
+    i = 0
+    while True:
+      n = 2*i if self.even else 2*i + 1
+      with self.lock:
+        print(f"Thread {self.even} is running. Lock status: {self.lock.locked()}")
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        sock.send(dict(n)).encode()
+        response = sock.recv(1024)
+        print(response.decode())
+      print(f"Thread {self.even} is sleeping. Lock status: {self.lock.locked()}")
+      i += 1
+      time.sleep(random.random())
+
+def run_threads():
+  lock = threading.Lock() 
+
+  producerEven = Producer(even=True, lock=lock)
+  producerOdd = Producer(even=False, lock=lock)
+
+  threadEven = threading.Thread(target=producerEven.send_data)
+  threadOdd = threading.Thread(target=producerOdd.send_data)
+  threadEven.start()
+  threadOdd.start()
+  threadEven.join()
+  threadOdd.join()
 
 if __name__ == "__main__":
-  print("Create producer")
+  run_threads()
